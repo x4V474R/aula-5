8d552488a6cd5819a1a9e44af0641148767a9c80
x4V474R <40581802+x4V474R@users.noreply.github.com>
2019-09-03 13:06:36 -0700

    Add files via upload

---
+++ aula5.py
@@ -0,0 +1,17 @@
+
+
+a = int(input("digite um numero"))
+b = int(input("digite outro numero"))
+c = int(input("digite outro numero"))
+
+print(b*b - 4 *a*c==x)
+
+if a > b:
+	print("o primeiro numero for maior")
+elif a < b:
+	print("o segundo numero e maior")
+elif a ==b:
+	print("os dois sao iguais")
+
+
+int()
